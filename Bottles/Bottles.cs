1f00e374ed630b27f126163791769547a86efb1c
Szymon Kazmierczak <szymonkaz@gmail.com>
2025-06-19 12:21:53 +0100

    finished chapter 4

--- Bottles/Bottles.cs
+++ Bottles/Bottles.cs
@@ -6,26 +6,11 @@ public class Bottles
 {
     public string Song() => Verses(99, 0);
 
-    public string Verse(int count)
-    {
-        return count switch
-        {
-            0 => "No more bottles of beer on the wall, " +
-                 "no more bottles of beer.\n" +
-                 "Go to the store and buy some more, " +
-                 "99 bottles of beer on the wall.\n",
-
-            1 => "1 bottle of beer on the wall, " +
-                 "1 bottle of beer.\n" +
-                 "Take it down and pass it around, " +
-                 "no more bottles of beer on the wall.\n",
-            
-            _ => $"{count} bottles of beer on the wall, " +
-                 $"{count} bottles of beer.\n" +
-                 "Take one down and pass it around, " +
-                 $"{count - 1} {Container(count - 1)} of beer on the wall.\n"
-        };
-    }
+    public string Verse(int number)
+        => $"{Capitalize(Quantity(number))} {Container(number)} of beer on the wall, " +
+           $"{Quantity(number)} {Container(number)} of beer.\n" +
+           Action(number) +
+           $"{Quantity(Successor(number))} {Container(Successor(number))} of beer on the wall.\n";
 
     public string Verses(int upper, int lower)
     {
@@ -46,4 +31,40 @@ public class Bottles
             1 => "bottle",
             _ => "bottles"
         };
+
+    private string Pronoun(int number) =>
+        number switch
+        {
+            1 => "it",
+            _ => "one"
+        };
+
+    private string Quantity(int number)
+        => number switch
+        {
+            0 => "no more",
+            _ => number.ToString()
+        };
+
+
+    private static string Capitalize(string input) =>
+        input switch
+        {
+            null => throw new ArgumentNullException(nameof(input)),
+            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+        };
+
+    private string Action(int number)
+        => number switch
+        {
+            0 => "Go to the store and buy some more, ",
+            _ => $"Take {Pronoun(number)} down and pass it around, "
+        };
+
+    private int Successor(int number) => number switch
+    {
+        0 => 99,
+        _ => number - 1
+    };
 }
